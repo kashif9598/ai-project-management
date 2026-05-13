d893e0fa4276e078b516ae94d66cec5133d7b007
kashif9598 <kashifsheikh9598@gmail.com>
2026-05-13 23:45:05 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return(
+    <div>
+      <p>AI Project Management</p>
+      <Button variant="outline">Get Started</Button>
+    </div>
+  )
+}
+export default App;
